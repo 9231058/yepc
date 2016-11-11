ea26ca4ed13f319e386268b48343ebca1bf3e2ef
Parham Alvani <parham.alvani@gmail.com>
2016-11-11 13:27:54 +0330

    Debug mode was disabled.

--- yepc/http/route.py
+++ yepc/http/route.py
@@ -68,6 +68,6 @@ def yacc_handler():
     l = lexer.build()
     p = parser.build()
 
-    p.parse(data, lexer=l, debug=True)
+    p.parse(data, lexer=l, debug=False)
 
     return json.dumps(result)
