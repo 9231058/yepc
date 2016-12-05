56fc964cb61738d5dc0d1ca536c96a5f9be45890
Parham Alvani <parham.alvani@gmail.com>
2016-12-06 02:17:47 +0330

    Quadruple class was added.

---
+++ yepc/core/qr.py
@@ -0,0 +1,16 @@
+# In The Name Of God
+# ========================================
+# [] File Name : qr.py
+#
+# [] Creation Date : 06-12-2016
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+
+
+class QuadRuple:
+    def __init__(self, op, arg_one, arg_two, result):
+        self.op = op
+        self.arg_one = arg_one
+        self.arg_two = arg_two
+        self.result = result
