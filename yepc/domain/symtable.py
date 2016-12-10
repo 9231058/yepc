55d335eb7632a53a38d3278b164696eee64461ba
Parham Alvani <parham.alvani@gmail.com>
2016-12-10 04:51:49 +0330

    Symbol table was improved to support tree architecture.

--- yepc/domain/symtable.py
+++ yepc/domain/symtable.py
@@ -17,10 +17,23 @@ class SymbolTable():
     | jj1    | int  |
 
     '''
-    def __init__(self):
+    def __init__(self, parent):
         self.seq = 0
         self.symbols = {}
+        self.header = {}
+        self.parent = parent
 
-    def new_temp(self, type):
+    def new_temp(self, temp_type):
         self.seq += 1
-        return 'jj' + self.seq
+        temp_id = 'jj' + self.seq
+        self.symbols[temp_id] = temp_type
+        return temp_id
+
+    def insert_variable(self, var_id, var_type):
+        self.symbols[var_id] = var_type
+
+    def insert_procedure(self, proc_id, proc_table):
+        self.symbols[proc_id] = proc_table
+
+    def add_width(self, width):
+        self.header[width] = width
