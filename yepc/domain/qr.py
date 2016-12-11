431daa1019e62d2780693ffc580a29f9b877cea4
saman <samanf74@gmail.com>
2016-12-11 16:44:19 +0330

    a lil bug fixed

--- yepc/domain/qr.py
+++ yepc/domain/qr.py
@@ -9,8 +9,8 @@
 
 
 class QuadRuple:
-    def __init__(self, op, arg1, arg_two, result):
+    def __init__(self, op, arg1, arg2, result):
         self.op = op
-        self.arg_one = arg_one
-        self.arg_two = arg_two
+        self.arg_one = arg1
+        self.arg_two = arg2
         self.result = result
