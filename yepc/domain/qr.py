c399d5086443b82bf386041f1598b6ab824c1a4d
Parham Alvani <parham.alvani@gmail.com>
2016-12-24 06:41:46 +0330

    Merge and backpatch functions were removed from quadruple.

--- yepc/domain/qr.py
+++ yepc/domain/qr.py
@@ -9,21 +9,10 @@
 # [] Created By : Saman Fekri (samanf74@gmail.com)
 # =======================================
 
-import copy
+
 class QuadRuple:
     def __init__(self, op, arg1, arg2, result):
         self.op = op
         self.arg_one = arg1
         self.arg_two = arg2
         self.result = result
-
-    @staticmethod
-    def backpatch(list, label):
-        for i in list:
-            i.arg_one = label
-
-    @staticmethod
-    def merge(list1,list2):
-        temp = copy.deepcopy(list1)
-        temp = temp + list2
-        return temp
