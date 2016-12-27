585944cec687b14cbaf6c7b473a772990ca3481c
Parham Alvani <parham.alvani@gmail.com>
2016-12-27 11:57:35 +0330

    Symbol table searching functions were added.

--- yepc/domain/symtable.py
+++ yepc/domain/symtable.py
@@ -25,11 +25,18 @@ class SymbolTable:
         self.meta = {}
         self.parent = parent
         self.temp_id_generator = self.temp_id_generator()
+        self.scope_id_generator = self.scope_id_generator()
 
     def temp_id_generator(self):
         seq = 0
         while True:
             yield 'jj' + str(seq)
+            seq += 1
+
+    def scope_id_generator(self):
+        seq = 0
+        while True:
+            yield 'js' + str(seq)
             seq += 1
 
     def new_temp(self, temp_type):
@@ -39,6 +46,10 @@ class SymbolTable:
 
     def insert_variable(self, var_id, var_type: str):
         self.symbols[var_id] = var_type
+
+    def insert_scope(self, scope_table):
+        temp_id = next(self.scope_id_generator)
+        self.symbols[temp_id] = scope_table
 
     def insert_procedure(self, proc_id, proc_table,
                          start=0, params=[], return_type='void'):
@@ -51,3 +62,25 @@ class SymbolTable:
 
     def add_width(self, width):
         self.header[width] = width
+
+    def get_symbol(self, symbol):
+        current = self
+        result = current.symbols.get(symbol, None)
+        while result is None:
+            if current.parent is not None:
+                current = current.parent
+            else:
+                raise KeyError(symbol)
+            result = current.symbols.get(symbol, None)
+        return result
+
+    def get_symbol_meta(self, symbol):
+        current = self
+        result = current.meta.get(symbol, None)
+        while result is None:
+            if current.parent is not None:
+                current = current.parent
+            else:
+                raise KeyError(symbol)
+            result = current.meta.get(symbol, None)
+        return result
