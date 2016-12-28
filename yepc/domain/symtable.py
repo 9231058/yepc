c6abdb2de048e44041b109cacfdc16c22df2df8c
Parham Alvani <parham.alvani@gmail.com>
2016-12-28 13:34:25 +0330

    Provides name generator for symbol table.

--- yepc/domain/symtable.py
+++ yepc/domain/symtable.py
@@ -53,8 +53,9 @@ class SymbolTable:
     def insert_scope(self, scope_table):
         self.symbols[scope_table.name] = scope_table
 
-    def insert_procedure(self, proc_id, proc_table,
+    def insert_procedure(self, proc_table,
                          start=0, params=[], return_type='void'):
+        proc_id = proc_table.name
         self.symbols[proc_id] = proc_table
         self.meta[proc_id] = {
             'start': start,
@@ -62,7 +63,7 @@ class SymbolTable:
             'return_type': return_type
         }
 
-    def get_symbol(self, symbol):
+    def get_symbol_type(self, symbol):
         current = self
         result = current.symbols.get(symbol, None)
         while result is None:
@@ -72,6 +73,17 @@ class SymbolTable:
                 raise KeyError(symbol)
             result = current.symbols.get(symbol, None)
         return result
+
+    def get_symbol_name(self, symbol):
+        current = self
+        result = current.symbols.get(symbol, None)
+        while result is None:
+            if current.parent is not None:
+                current = current.parent
+            else:
+                raise KeyError(symbol)
+            result = current.symbols.get(symbol, None)
+        return current.generate_symbol_name(symbol)
 
     def get_symbol_meta(self, symbol):
         current = self
@@ -84,7 +96,7 @@ class SymbolTable:
             result = current.meta.get(symbol, None)
         return result
 
-    def get_symbol_name(self, name):
+    def generate_symbol_name(self, name):
         if name[0] == '#':
             name = name[1:]
         current = self
