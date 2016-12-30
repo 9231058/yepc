fa485b3458f92536d6d4f69b637db5dbeb2f3b94
Parham Alvani <parham.alvani@gmail.com>
2016-12-31 03:04:15 +0330

    Main controller was improved.

--- yepc-UI/app/scripts/controllers/main.js
+++ yepc-UI/app/scripts/controllers/main.js
@@ -8,5 +8,35 @@
  * Controller of the yepcUiApp
  */
 angular.module('yepcUiApp')
-  .controller('MainCtrl', function () {
-  });
+.controller('MainCtrl', function ($scope, YepcService) {
+  $scope.tokenize = function () {
+    YepcService.tokenize($scope.source).then(function (data) {
+      $scope.tokens = data;
+    });
+  };
+
+  $scope.toC = function () {
+    YepcService.code($scope.source).then(function (data) {
+      $scope.cSource = data;
+    });
+  };
+
+  $scope.generate = function () {
+    YepcService.generate($scope.source).then(function (data) {
+      var downloadLink = angular.element('<a></a>');
+      downloadLink.attr('href', 'download/' + data);
+      downloadLink[0].click();
+    });
+  };
+
+  $scope.fromFile = function () {
+    var file = document.getElementById('file').files[0];
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      $scope.source = e.target.result;
+    };
+    if (file) {
+      reader.readAsText(file);
+    }
+  };
+});
