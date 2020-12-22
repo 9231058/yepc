938a9a9bcd1393c17a3ee2b4cb4a14604ddf6f0f
Parham Alvani <parham.alvani@gmail.com>
2020-12-22 09:53:55 +0330

    feat: Change Routing Logic

--- yepc/http/route.py
+++ yepc/http/route.py
@@ -22,28 +22,31 @@ lexer = YEPCLexer()
 
 # About
 
-@app.route('/about')
+
+@app.route("/about")
 def about_handler():
-        return "18.20 is leaving us"
+    return "18.20 is leaving us"
 
 
 # UI
 
-@app.route('/<path:path>', methods=['GET'])
+
+@app.route("/<path:path>", methods=["GET"])
 def ui_handler(path):
-    return flask.send_from_directory('yepc-UI/dist', path)
+    return flask.send_file("yepc-UI/dist/" + path)
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def root_handler():
-    return flask.send_file('yepc-UI/dist/index.html')
+    return flask.send_file("yepc-UI/dist/index.html")
 
 
 # Lex Phase
 
-@app.route('/lex', methods=['POST'])
+
+@app.route("/lex", methods=["POST"])
 def lex_handler():
-    data = flask.request.data.decode('ASCII')
+    data = flask.request.data.decode("ASCII")
 
     result = []
 
@@ -52,26 +55,26 @@ def lex_handler():
     l.input(data)
 
     for tok in l:
-        result.append({
-            'type': tok.type,
-            'value': tok.value,
-            'lineno': tok.lineno,
-            'lexpos': tok.lexpos
-        })
+        result.append(
+            {
+                "type": tok.type,
+                "value": tok.value,
+                "lineno": tok.lineno,
+                "lexpos": tok.lexpos,
+            }
+        )
 
     return json.dumps(result)
 
 
 # Parse + Interintermediate Code Generation Phase
 
-@app.route('/yacc', methods=['POST'])
-def yacc_handler():
-    data = flask.request.data.decode('ASCII')
 
-    results = {
-        'quadruples': [],
-        'symtables': {}
-    }
+@app.route("/yacc", methods=["POST"])
+def yacc_handler():
+    data = flask.request.data.decode("ASCII")
+
+    results = {"quadruples": [], "symtables": {}}
 
     parser = YEPCParser()
 
@@ -82,14 +85,14 @@ def yacc_handler():
 
     for q in parser.quadruples:
         result = {}
-        result['result'] = q.result
-        result['op'] = q.op
-        result['arg_one'] = q.arg_one
-        result['arg_two'] = q.arg_two
-        results['quadruples'].append(result)
+        result["result"] = q.result
+        result["op"] = q.op
+        result["arg_one"] = q.arg_one
+        result["arg_two"] = q.arg_two
+        results["quadruples"].append(result)
 
     symtables = {}
-    symtables['root'] = parser.symtables[0]
+    symtables["root"] = parser.symtables[0]
 
     while bool(symtables):
         name, table = symtables.popitem()
@@ -100,14 +103,14 @@ def yacc_handler():
             elif isinstance(tv, SymbolTable):
                 symtables[tk] = tv
                 result[tk] = tv.type
-        results['symtables'][name] = result
+        results["symtables"][name] = result
 
     return json.dumps(results)
 
 
-@app.route('/code', methods=['POST'])
+@app.route("/code", methods=["POST"])
 def code_handler():
-    data = flask.request.data.decode('ASCII')
+    data = flask.request.data.decode("ASCII")
 
     parser = YEPCParser()
 
@@ -121,9 +124,9 @@ def code_handler():
     return c_generator.to_c()
 
 
-@app.route('/generate', methods=['POST'])
+@app.route("/generate", methods=["POST"])
 def generate_handler():
-    data = flask.request.data.decode('ASCII')
+    data = flask.request.data.decode("ASCII")
 
     parser = YEPCParser()
 
@@ -132,18 +135,18 @@ def generate_handler():
 
     p.parse(data, lexer=l, debug=False)
 
-    tar_name = 'yepc-%d-1820.tar.gz' % int(time.time()*100)
+    tar_name = "yepc-%d-1820.tar.gz" % int(time.time() * 100)
 
-    tar = tarfile.open('out/%s' % tar_name, 'w:gz')
-    tar.add('out/main.c', 'yepc-output/main.c')
-    tar.add('assets/stack.h', 'yepc-output/stack.h')
-    tar.add('assets/stack.c', 'yepc-output/stack.c')
-    tar.add('assets/Makefile', 'yepc-output/Makefile')
+    tar = tarfile.open("out/%s" % tar_name, "w:gz")
+    tar.add("out/main.c", "yepc-output/main.c")
+    tar.add("assets/stack.h", "yepc-output/stack.h")
+    tar.add("assets/stack.c", "yepc-output/stack.c")
+    tar.add("assets/Makefile", "yepc-output/Makefile")
     tar.close()
 
     return tar_name
 
 
-@app.route('/download/<string:name>', methods=['GET'])
+@app.route("/download/<string:name>", methods=["GET"])
 def download_handler(name):
-    return flask.send_from_directory('out', name)
+    return flask.send_from_directory("out", name)
